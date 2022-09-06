bd10f32625d8c367d0987bae299156832454bdf5
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-09-06 13:08:49 +0900

    Fix light mode colors

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const darkMode = document.querySelector('.dark-mode');
 darkMode.addEventListener('click', (e) => {
     console.log(e.target.innerHTML);
     if (e.target.innerHTML === 'Light Mode <i class="fa-solid fa-sun"></i>') {
-        document.documentElement.style.setProperty('--main-background', '#ccccccf8');
-        document.documentElement.style.setProperty('--secondary-background', '#ccccccfa');
-        document.documentElement.style.setProperty('--content', '#cccccc');
+        document.documentElement.style.setProperty('--main-background', '#eeeeeef8');
+        document.documentElement.style.setProperty('--secondary-background', '#eeeeeef5');
+        document.documentElement.style.setProperty('--content', '#eeeeeef1');
         document.documentElement.style.setProperty('--main-text', '#0030ce');
         document.documentElement.style.setProperty('--secondary-text', '#0028ad');
         document.documentElement.style.setProperty('--content-text', '#00196d');
